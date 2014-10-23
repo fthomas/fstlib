524a458a3ee4ba9ab184d4a540e33c9241ff7990
Frank S. Thomas <frank@timepit.eu>
2014-10-23 06:26:49 +0200

    More proofs

--- tree.v
+++ tree.v
@@ -22,6 +22,27 @@ Fixpoint depth {X : Type} (t : tree X) : nat :=
   | node _ l r => 1 + max (depth l) (depth r)
   end.
 
+Theorem tree_size_min : forall {X : Type} (t : tree X),
+  depth t <= size t.
+Proof.
+  intros. induction t.
+    simpl. reflexivity.
+    simpl. apply Le.le_n_S. apply Nat.max_lub.
+      apply Plus.le_plus_trans. auto.
+      rewrite Plus.plus_comm.
+      apply Plus.le_plus_trans. auto.
+Qed.
+
+SearchAbout mult.
+
+Theorem tree_size_max : forall {X : Type} (t : tree X),
+  size t + 1 <= 2 ^ (depth t).
+Proof.
+  intros. induction t.
+    simpl. reflexivity.
+    simpl. rewrite <- plus_n_O. apply Nat.max_lub.
+
+
 Inductive non_empty {X : Type} : tree X -> Prop :=
   non_empty_node : forall x l r, non_empty (node x l r).
 
@@ -34,22 +55,35 @@ Inductive full {X : Type} : tree X -> Prop :=
 
 Inductive perfect {X : Type} : tree X -> Prop :=
   | perfect_empty : perfect empty
-  | perfect_leaf : forall x, perfect (leaf x)
   | perfect_node : forall x l r,
-      non_empty l -> full l ->
-      non_empty r -> full r ->
-      size l = size r -> perfect (node x l r).
+      non_empty l -> perfect l ->
+      non_empty r -> perfect r -> size l = size r -> perfect (node x l r).
 
 Theorem perfect_implies_full : forall {X : Type} (t : tree X),
   perfect t -> full t.
 Proof.
   intros. induction H.
-    constructor. constructor. constructor.
-    auto. auto. auto. auto. 
+    constructor. constructor.
+    auto. auto. auto. auto.
 Qed.
 
 Theorem size_of_perfect : forall {X : Type} (t : tree X),
   perfect t -> size t = 2 ^ (depth t) - 1.
+Proof.
+  intros. induction H.
+    simpl. reflexivity.
+    
+    simpl. rewrite IHt1. rewrite IHt2. simpl. rewrite IHperfect2.
+    simpl.
+    
+      simpl. reflexivity.
+
+
+    unfold size. simpl.
+    destruct H.
+      simpl. reflexivity.
+      simpl. reflexivity.
+Admitted.
 
 (** Tests **)
 
